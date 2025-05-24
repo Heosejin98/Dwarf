9e8c50ef44b1e244226058f37d2cada84bd25a0a
sejin <61305083+Heosejin98@users.noreply.github.com>
2025-05-24 20:53:44 +0900

    테스트용 모듈 추가 #7 (#8)
    * feat: add test-was
    
    * feat: update test-was Spring Boot version and add application configuration

---
+++ test-was/src/main/java/org/dwarf/testwas/TestWasApplication.java
@@ -0,0 +1,12 @@
+package org.dwarf.testwas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestWasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestWasApplication.class, args);
+    }
+}
